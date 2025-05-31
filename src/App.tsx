89323c2c22203bf75892d4b120d4a0d1ea278c0c
Aluminium <yuening.zhang100@gmail.com>
2025-05-31 12:05:39 +0800

    move top button to bottom to avoid getting block

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,9 @@ function App() {
     
     {/* navigation frame at top, dark blue */}
     <header className="sticky top-0 bg-blue-800 shadow-md z-40">
-      <div className="container mx-auto px-5 py-5 flex justify-between items-center">
-        {/* title + menu button */}
+      <div className="container mx-auto px-5 py-4 flex justify-between items-center">
+        {/* title */}
         <h1 className="text-xl font-bold text-white">CDE Map</h1>
-        <button id="menuBtn" className="text-white">
-          <i className="fa-solid fa-bars text-xl"></i>
-        </button>
         {/* input frame + search button*/}
         <div className="absolute top-15 left-1/2 transform -translate-x-1/2 w-11/12 max-w-xl shadow-lg flex items-center space-x-2">
           <input
@@ -38,8 +35,7 @@ function App() {
           />
           <button
             onClick={handleSearch}
-            className="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded-lg transition-colors touch-manipulation active:scale-95"
-          >
+            className="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded-lg transition-colors touch-manipulation active:scale-95">
           <i className="fa-solid fa-magnifying-glass"></i>
           </button>
         </div>
@@ -48,15 +44,18 @@ function App() {
     
     {/* image of CDE map */}
     <div className="h-[40vh] flex justify-center items-end" style={{ transform: 'translateY(250px)' }}>
-      <img src="/CDEmap.png" alt="CDE Map" style={{ width: '450px', height: '450px' }}/>
+      <img src="/CDEmap.png" alt="CDE Map" style={{ width: '500px', height: '500px' }}/>
     </div>
 
       {/* + - buttons */}
       <div className="absolute bottom-6 right-6 flex flex-col space-y-3 z-10">
-        <button className="w-12 h-12 rounded-full bg-white shadow-lg flex items-center justify-center hover:bg-gray-100">
+        <button id="menuBtn" className="w-12 h-12 rounded-full bg-white shadow-lg flex items-center justify-center hover:bg-gray-100 transition-colors touch-manipulation active:scale-95">
+          <i className="fa-solid fa-bars text-xl"></i>
+        </button>
+        <button className="w-12 h-12 rounded-full bg-white shadow-lg flex items-center justify-center hover:bg-gray-100 transition-colors touch-manipulation active:scale-95">
           <i className="fa-solid fa-plus text-dark"></i>
         </button>
-        <button className="w-12 h-12 rounded-full bg-white shadow-lg flex items-center justify-center hover:bg-gray-100">
+        <button className="w-12 h-12 rounded-full bg-white shadow-lg flex items-center justify-center hover:bg-gray-100 transition-colors touch-manipulation active:scale-95">
           <i className="fa-solid fa-minus text-dark"></i>
         </button>
       </div>
