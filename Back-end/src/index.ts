76c369ad1b26de49b1f33ff6b55a7125e2b34c70
Aluminium <yuening.zhang100@gmail.com>
2025-06-17 10:10:05 +0800

    back end frame starter

---
+++ Back-end/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express + TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
