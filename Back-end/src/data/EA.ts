4c978d7f75075b01f052ce671028283008c3f598
Aluminium <yuening.zhang100@gmail.com>
2025-06-27 20:12:54 +0800

    update

--- Back-end/src/data/EA.ts
+++ Back-end/src/data/EA.ts
@@ -8,11 +8,11 @@ export const EA: Building = {
       { id: "EA-01-05", x: 765, y: 723, floor: 1 },
       { id: "EA-01-06", x: 806, y: 723, floor: 1 },
       { id: "EA-01-L1", x: 887, y: 723, floor: 1 }
-      
+      //L2
 
     ],
     edges: [
       { from: "A1", to: "A2", distance: 40 },
       { from: "A2", to: "A3", distance: 40 }
-    ]
+    ] 
   };
