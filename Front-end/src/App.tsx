56dacde229b03cc9db0c3975786a811f22f19a73
Aluminium <yuening.zhang100@gmail.com>
2025-07-01 22:23:27 +0800

    Remove vertical scroll bar + input back to typing

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -16,79 +16,53 @@ function App() {
 
   return(
   <>
-    {/* background */}
-    <div className="bg-white min-h-screen font-sans">
-    
     {/* navigation frame*/}
-    <header className="sticky top-0 bg-blue-800 shadow-md z-40">
+    <header className="fixed top-0 left-0 right-0 bg-blue-800 shadow-md z-50">
       <div className="container mx-auto px-5 py-0">
         {/* title */}
         <div className="flex justify-left py-3 px-6">
         <h1 className="text-xl font-bold text-white">CDE Map</h1>
         </div>
-          </div>
+      </div>
     </header>
 
-        {/* input frame + search button*/}
-        <div className=" bg-blue-400 w-full shadow-md">
-        <div className="grid grid-cols-4 gap-5 pl-5 pr-8 py-3">
-          <label htmlFor="building" className="text-white text-xl font-medium mb-1 flex justify-center items-center">
+    {/* input frame + search button*/}
+    <div className="fixed top-12  left-0 right-0 bg-blue-400 w-full shadow-md py-3 z-40 ">
+      <div className="flex items-center pt-1 px-5">
+        <label htmlFor="building" className="w-32 text-white text-xl font-medium mb-1 flex justify-center items-center">
           Destination:
         </label>
-        {/*building selection*/}
-          <select
-            className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-800"
-            value={destination}
-            onChange={(e) => setDestination(e.target.value)}>
-            <option value="" disabled> Block </option>
-            <option value="EA">EA</option>
-            <option value="E1">E1</option>
-            <option value="E2">E2</option>
-            <option value="E2">E3</option>
-          </select>
-          {/*floor selection*/}
-          <select
-            value = {currentFloor}
-            className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-800"
-            onChange={(e) => setCurrentFloor(e.target.value)}>
-            <option value="" disabled> Floor </option>
-            <option value="L1">L1</option>
-            <option value="L2">L2</option>
-            <option value="L3">L3</option>
-            <option value="L4">L4</option>
-            <option value="L5">L5</option>
-            <option value="L6">L6</option>
-            <option value="L7">L7</option>
-          </select>
-          {/*room selection*/}
-          <select
-            className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-800"
-            value={destination}
-            onChange={(e) => setDestination(e.target.value)}>
-            <option value="" disabled> Room </option>
-            <option value="01">01</option>
-            <option value="02">02</option>
-            <option value="03">03</option>
-            <option value="04">04</option>
-          </select>
-        </div>
-        </div>
-
-
-
-      {/* menu button */}
-      <div className="absolute bottom-6 right-6 flex flex-col space-y-3 z-10">
-        <button id="menuBtn" className="w-12 h-12 z-20 rounded-full bg-white shadow-lg flex items-center justify-center hover:bg-gray-100 transition-colors touch-manipulation active:scale-95">
-          <i className="fa-solid fa-bars text-xl"></i>
+        <input
+          className="flex-1 ml-5 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-800"
+          value={destination}
+          type = "text"
+          onChange={(e) => setDestination(e.target.value)}>
+        </input>
+        <button
+          onClick={handleSearch}
+          className="w-15 mr-5 ml-5 bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded-lg transition-colors touch-manipulation active:scale-95">
+          <i className="fa-solid fa-magnifying-glass"></i>
         </button>
-       
+      </div>
+    </div>
+
+ {/* background */}
+    <div className="fixed top-28 left-0 right-0 bottom-20 bg-white z-10">
+  {/* CDE map leaflet */}
+      <div className = "h-full w-full">
+        <MapComponent 
+      destination={destination}
+      currentFloor={currentFloor}
+      />
+      </div>
       </div>
 
-      {/* CDE map leaflet */}
-      <MapComponent 
-      destination={destination}
-      currentFloor={currentFloor}/>
-    </div>
+      {/* menu button */}
+      <div className="fixed bottom-6 right-6 flex flex-col space-y-3 z-10">
+        <button id="menu" className="w-12 h-12 z-20 rounded-full bg-white shadow-lg flex items-center justify-center hover:bg-gray-100 transition-colors touch-manipulation active:scale-95">
+          <i className="fa-solid fa-bars text-xl"></i>
+        </button>
+      </div>
     </>
   );
 }
